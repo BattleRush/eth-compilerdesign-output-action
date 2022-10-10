e51c7b084f219f30b252c8c08421558cb3774adf
Karlo Piskor <kpiskor@student.ethz.ch>
2022-10-10 20:44:18 +0200

    latest changes

--- dist/index.js
+++ dist/index.js
@@ -42515,7 +42515,8 @@ try {
                     var projectName = line.substring(line.lastIndexOf('/') + 1, line.lastIndexOf('\''));
                     currentProject.name = projectName;
                     currentProject.folderName = projectName;
-
+                    var isInvalidProject = false;
+                    /* TODO SET SCORE FOR PROJECT WHEN RELEASED */
                     switch (currentProject.maxScore) {
                         case 67:
                             currentProject.name = "Project 1: Hellocaml";
@@ -42523,27 +42524,28 @@ try {
                         case 46:
                             currentProject.name = "Project 2: x86Lite";
                             break;
-                        default:
+                        case -1:
                             currentProject.name = "Project 3: Compiling LLVM";
                             break;
-
-                            // TO Come
-
-                            /*                    case 4:
-                        projectName = "Project 4: Compiling Oat v.1";
-                        maxScore = -1;
-                        break;
-                    case 5:
-                        projectName = "Project 5: Compiling Full Oat";
-                        maxScore = 80;
-                        break;
-                    case 6:
-                        projectName = "Project 6: Dataflow Analysis and Register Allocation";
-                        maxScore = 100;
-                        break; */
+                        case -2:
+                            currentProject.name = "Project 4: Compiling Oat v.1";
+                            break;
+                        case 80:
+                            currentProject.name = "Project 5: Compiling Full Oat";
+                            break;
+                        case 100:
+                            currentProject.name = "Project 6: Dataflow Analysis and Register Allocation";
+                            break; 
+                            
+                        default:
+                            isInvalidProject = true;
+                            break;
+                            
                     }
-
-                    projectResults.push(currentProject);
+                    
+                    if(!isInvalidProject)
+                        projectResults.push(currentProject);
+                    
                     break;
                 }
 
@@ -42804,6 +42806,7 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
+
 })();
 
 module.exports = __webpack_exports__;
